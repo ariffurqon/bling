4efc59fc3580d298ba61ffceb9b506a825fac6bf
ariffurqon <arif.furqon@gmail.com>
2015-12-05 02:31:47 -0800

    controlling auth on pages

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 var myApp = angular.module('angieauth', ['ngRoute', 'firebase'])
 	.constant('FIREBASE_URL', 'https://angiebling.firebaseio.com/')
 
+myApp.run(['$rootScope', '$location', function($rootScope, $location){
+	$rootScope.$on('$routeChangeError', function(event, next, previous, error) {
+		if (error=='AUTH_REQUIRED') {
+			$rootScope.message = 'Sorry, you must log in to access that page';
+			$location.path('/login');
+		} // AUTH ReQUIRED
+	}); // event info
+}]); // run
+
 myApp.config(['$routeProvider', function($routeProvider){
 	$routeProvider
 		.when('/login', {
@@ -14,7 +23,12 @@ myApp.config(['$routeProvider', function($routeProvider){
 		})
 		.when('/success', {
 			templateUrl: 'views/success.html',
-			controller: 'successCtrl'
+			controller: 'successCtrl',
+			resolve: {
+				currentAuth: function(Authentication) {
+					return Authentication.requireAuth();
+				} // current Auth
+			} // resolve
 		})
 		.otherwise({
 			redirectTo: '/login'

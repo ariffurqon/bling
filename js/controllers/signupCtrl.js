81b671223fa2a33cba8935550320a4905e0e4d40
ariffurqon <arif.furqon@gmail.com>
2015-12-04 22:12:10 -0800

    processing form input

--- js/controllers/signupCtrl.js
+++ js/controllers/signupCtrl.js
@@ -1,3 +1,12 @@
 myApp.controller('signupCtrl', ['$scope', function($scope) {
- 	$scope.test = "Hello lovers...";
+ 	// $scope.test = "Hello lovers...";
+
+ 	$scope.login = function() {
+ 		$scope.message = "Welcome" + $scope.user.email;
+ 	};
+
+	$scope.signup = function() {
+		$scope.message = "Welcome" + $scope.user.firstname;
+	}; 	
+
  }]);

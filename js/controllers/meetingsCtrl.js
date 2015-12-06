5bdab243d2e918b07dd31be3cfd178dd91462415
ariffurqon <arif.furqon@gmail.com>
2015-12-05 23:56:39 -0800

    adding records to the db

--- js/controllers/meetingsCtrl.js
+++ js/controllers/meetingsCtrl.js
@@ -1,3 +1,29 @@
-myApp.controller('meetingsCtrl', ['$scope', function($scope) {
- 	$scope.test = "oh hey!!!";
- }]);
+myApp.controller('meetingsCtrl', 
+	['$scope', 
+	'$rootScope', 
+	'$firebaseAuth',
+	'$firebaseArray',
+	'FIREBASE_URL',
+	function($scope, $rootscope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
+ 		
+		var ref = new Firebase(FIREBASE_URL);
+		var auth = $firebaseAuth(ref);
+
+		auth.$onAuth(function(authUser) {
+			if (authUser) {
+				var meetingsRef = new Firebase(FIREBASE_URL + 'users/' + 
+					$rootscope.currentUser.$id + '/meetings');
+				var meetingsInfo = $firebaseArray(meetingsRef);
+
+				$scope.addMeeting = function() {
+					meetingsInfo.$add({
+						name: $scope.meetingname,
+						date: Firebase.ServerValue.TIMESTAMP
+					}).then (function() {
+						$scope.meetingname = '';
+					}); // promise
+				}; // add meeting
+			} // user authenticated
+		}); // on Auth
+
+ }]); // controller
